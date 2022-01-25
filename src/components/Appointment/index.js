105f9515f26fafb0794f2684c8612ded1baabd12
Mikko De Los Reyes <mikkode212@gmail.com>
2022-01-25 14:44:01 -0500

    this commit now implements the edit appoointments button

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,7 +8,6 @@ import Form from "components/Appointment/Form";
 import Status from "components/Appointment/Status";
 import Confirm from "components/Appointment/Confirm";
 
-
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
@@ -16,57 +15,68 @@ export default function Appointment(props) {
   const SAVING = "SAVING";
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
-    );
+  );
 
   function save(name, interviewer) {
     const interview = {
       student: name,
-      interviewer
+      interviewer,
     };
 
     transition(SAVING);
-    props.bookInterview(props.id, interview)
-    .then(() => transition(SHOW));
+    props.bookInterview(props.id, interview).then(() => transition(SHOW));
   }
 
   function deleteInterview() {
     transition(DELETING, true);
-    props.cancelInterview(props.id)
-    .then(() => transition(EMPTY))
+    props.cancelInterview(props.id).then(() => transition(EMPTY));
   }
-
 
   return (
     <article className="appointment">
-        <Header time={props.time} />
-        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-        {mode === SHOW && (
-          <Show
-            student={props.interview.student}
-            interviewer={props.interview.interviewer}
-            onDelete={() => transition(CONFIRM)}
-          />
-        )}
-        {mode === SAVING && <Status message ="Saving..." />}
-        {mode === DELETING && <Status message ="Deleting..."/>}
-        {mode === CONFIRM && <Confirm message="Are you sure you want to delete the appointment?"
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
+        />
+      )}
+      {mode === SAVING && <Status message="Saving..." />}
+      {mode === DELETING && <Status message="Deleting..." />}
+      {mode === CONFIRM && (
+        <Confirm
+          message="Are you sure you want to delete the appointment?"
           onConfirm={deleteInterview}
-          onCancel ={back}
-          />}
+          onCancel={back}
+        />
+      )}
 
-        {mode === CREATE && (
-          <Form
+      {mode === CREATE && (
+        <Form
           name={props.name}
           interviewers={props.interviewers}
           value={props.value}
           onCancel={() => back(EMPTY)}
           onSave={save}
-
-          />
-        )}
+        />
+      )}
+      
+      {mode === EDIT && (
+        <Form
+          student={props.interview.student}
+          value={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onSave={save}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   );
 }
