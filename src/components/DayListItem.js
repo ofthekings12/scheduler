e32a0102ec3545e2c3bc48c3a658b439f145c6a5
Mikko De Los Reyes <mikkode212@gmail.com>
2022-01-27 15:03:34 -0500

    this commit implements cypress test for navigating days

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -22,7 +22,9 @@ export default function DayListItem(props) {
   }
 
   return (
-    <li className={dayClass} 
+    <li 
+    data-testid="day"
+    className={dayClass} 
     onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots()}</h3>
